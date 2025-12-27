031a24b2f34315cbda8e8238d27b8e9e0f42bd74
harry <harryantoniades1@gmail.com>
2025-12-27 19:32:53 +0000

    Remove unused constant

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Layout } from "./components/Layout/index";
